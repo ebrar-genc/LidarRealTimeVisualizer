1654be341f9294518e22abe1969973a1e21f0463
ebrar-genc <96254726+ebrar-genc@users.noreply.github.com>
2023-12-21 16:59:31 +0300

    Rece
    connecting to serial port
    
    Keeping data coming through serial port in LidarData class

---
+++ ldrobot/ldrobot/LidarData.cs
@@ -0,0 +1,94 @@
+﻿/// <summary>
+/// Represents the data structure for LIDAR data.
+/// </summary>
+public class LidarData
+{
+    #region Properties
+
+    /// <summary>
+    /// Gets or sets the header of the LIDAR data.
+    /// </summary>
+    public byte Header { get; private set; }
+
+    /// <summary>
+    /// Gets or sets the version and length information of the LIDAR data.
+    /// </summary>
+    public byte VersionLength { get; private set; }
+
+    /// <summary>
+    /// Gets or sets the speed information of the LIDAR data in degrees per second.
+    /// </summary>
+    public ushort Speed { get; private set; }
+
+    /// <summary>
+    /// Gets or sets the start angle of the LIDAR data in degrees.
+    /// </summary>
+    public ushort StartAngle { get; private set; }
+
+    /// <summary>
+    /// Gets or sets the data payload of the LIDAR data.
+    /// </summary>
+    public byte[] Data { get; private set; }
+
+    /// <summary>
+    /// Gets or sets the end angle of the LIDAR data in degrees.
+    /// </summary>
+    public ushort EndAngle { get; private set; }
+
+    /// <summary>
+    /// Gets or sets the timestamp of the LIDAR data in milliseconds.
+    /// </summary>
+    public ushort Timestamp { get; private set; }
+
+    /// <summary>
+    /// Gets or sets the CRC (Cyclic Redundancy Check) of the LIDAR data.
+    /// </summary>
+    public byte CRC { get; private set; }
+
+    #endregion Properties
+
+    #region Public Functions
+
+    /// <summary>
+    /// Creates a new instance of the LidarData class from raw byte data.
+    /// </summary>
+    /// <param name="rawData">The raw byte data representing LIDAR data.</param>
+    /// <returns>A LidarData object populated with parsed data.</returns>
+    public static LidarData FromRawData(byte[] rawData)
+    {
+        LidarData lidarData = new LidarData();
+
+        lidarData.Header = rawData[0];
+        lidarData.VersionLength = rawData[1];
+        lidarData.Speed = BitConverter.ToUInt16(rawData, 2);
+        lidarData.StartAngle = BitConverter.ToUInt16(rawData, 4);
+
+        int dataLength = lidarData.VersionLength & 0x1F;
+        lidarData.Data = new byte[dataLength * 3];
+        Array.Copy(rawData, 6, lidarData.Data, 0, lidarData.Data.Length);
+
+        int dataIndex = 6 + lidarData.Data.Length;
+        lidarData.EndAngle = BitConverter.ToUInt16(rawData, dataIndex);
+        lidarData.Timestamp = BitConverter.ToUInt16(rawData, dataIndex + 2);
+        lidarData.CRC = rawData[dataIndex + 4];
+
+        return lidarData;
+    }
+
+    /// <summary>
+    /// Returns a string representation of the LidarData object.
+    /// </summary>
+    /// <returns>A string containing formatted information about the LidarData object.</returns>
+    public override string ToString()
+    {
+        return $"Header: 0x{Header:X}\n" +
+               $"VersionLength: 0x{VersionLength:X}\n" +
+               $"Speed: {Speed} degrees per second\n" +
+               $"Start Angle: {StartAngle * 0.01} degrees\n" +
+               $"Data Length: {Data.Length} bytes\n" +
+               $"End Angle: {EndAngle * 0.01} degrees\n" +
+               $"Timestamp: {Timestamp} milliseconds\n" +
+               $"CRC: 0x{CRC:X}";
+    }
+    #endregion
+}
---
+++ ldrobot/ldrobot/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.IO.Ports;
+
+/// <summary>
+/// Represents a simple program for receiving and analyzing LIDAR data from a serial port.
+/// </summary>
+class Program
+{
+    static SerialPort serialPort;
+
+    static void Main()
+    {
+        string portName = "COM5";
+        int baudRate = 230400;
+
+        serialPort = new SerialPort(portName, baudRate);
+
+        serialPort.Open();
+
+        serialPort.DataReceived += SerialPort_DataReceived;
+
+        Console.WriteLine("Waiting for LIDAR data. Press Enter to exit.");
+        Console.ReadLine();
+
+        serialPort.Close();
+    }
+
+    /// <summary>
+    /// Event handler for the DataReceived event of the serial port.
+    /// </summary>
+    private static void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+    {
+        byte[] buffer = new byte[serialPort.BytesToRead];
+        serialPort.Read(buffer, 0, buffer.Length);
+
+        LidarData lidarData = LidarData.FromRawData(buffer);
+
+        Console.WriteLine(lidarData);
+
+        AppendToFile("lidar.txt", BitConverter.ToString(buffer));
+    }
+
+    /// <summary>
+    /// Appends text to a file.
+    /// </summary>
+    /// <param name="fileName">The name of the file to which text should be appended.</param>
+    /// <param name="text">The text to append to the file.</param>
+    private static void AppendToFile(string fileName, string text)
+    {
+        using (StreamWriter sw = File.AppendText(fileName))
+        {
+            sw.WriteLine(text);
+        }
+    }
+}
